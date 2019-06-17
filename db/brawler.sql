83eea5bc5acd9e425360052fda298bbba2794ddb
Felix <felix.martelin@etu.univ-lyon1.fr>
2019-06-17 19:10:15 +0200

    sql

--- db/brawler.sql
+++ db/brawler.sql
@@ -1,8 +1,8 @@
 create database if not exists Brawler character set utf8 collate utf8_unicode_ci;
 use Brawler;
 CREATE user if not exists 'felixMartelin'@'localhost' identified by 'secret';
-
 grant all privileges on Brawler.* to 'felixMartelin'@'localhost' identified by 'secret';
+
 create table Brawler (
 IdBrawler integer not null primary key auto_increment,
 NomBrawler varchar(45) not null,
